70f1683bd2ef6a136ec8c9d792bf3b63c2d6b0d8
Jumoke <jumoke.bolanle@gmail.com>
2020-02-04 22:54:36 -0500

    used setUp(self) to account for multiple test types

--- Chapter11/test_survey.py
+++ Chapter11/test_survey.py
@@ -4,23 +4,25 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for AnonymousSurvey class"""
 
+    def setUp(self):
+        """
+        Create a survey and a set of responses for use in all test methods
+        """
+        question = "What language did you first learnt to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Yoruba', 'French']
+
     def test_store_single_response(self):
         """Tests that a single response gets stored properly"""
         question = "What language did you first learnt to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_respones('English')
-
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_respones(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_multiple_responses(self):
         """Tests that more than one response gets stored properly"""
-        question = "What language did you first learnt to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Yoruba', 'French']
-        for response in responses:
-            my_survey.store_respones(response)
-
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_respones(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 unittest.main()
