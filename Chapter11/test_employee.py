9a39bb1e44923f6b3e982cbd44b8484d16d497e6
Jumoke <jumoke.bolanle@gmail.com>
2020-02-06 13:16:42 -0500

    tests for default give_raise() and custom give_raise()

--- Chapter11/test_employee.py
+++ Chapter11/test_employee.py
@@ -5,5 +5,16 @@ class TestGiveRaise(unittest.TestCase):
     """Tests default raise and custom raise"""
 
     def setUp(self):
-        """Tests give_raise() on an employee"""
+        """Sets up employee"""
         self.jumoke = Employee('jumoke', 'bolanle', 120000)
+
+    def test_default_raise(self):
+        """Tests if default raise works"""
+        self.jumoke.give_raise()
+        self.assertEqual(self.jumoke.annual_salary, 125000)
+
+    def test_custom_raise(self):
+        self.jumoke.give_raise(2000)
+        self.assertEqual(self.jumoke.annual_salary, 122000)
+
+unittest.main()
